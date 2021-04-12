6c42829dcfa5263f9a5c8f1de1996238b2ce2947
Alexey Serov (DevExpress) <Serov.Alexey@corp.devexpress.com>
2021-04-12 15:40:17 +0300

    add babel for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
-const path = require('path');
+import {resolve, dirname} from 'path';
+import { fileURLToPath } from 'url';
 
-module.exports = {
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+export default {
   entry: './src/entrypoint.js',  
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: resolve(__dirname, 'dist'),
     filename: 'bundle.js',       
   },
 };
